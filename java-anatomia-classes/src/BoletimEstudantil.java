ab9e7eb04b463976b03e6b6aeaef4ee55f52aa0a
jooao7 <jvitorfjv781@gmail.com>
2024-05-03 17:01:05 -0300

    refina exemplos de classes com novos métodos

--- java-anatomia-classes/src/BoletimEstudantil.java
+++ java-anatomia-classes/src/BoletimEstudantil.java
@@ -1,6 +1,6 @@
 public class BoletimEstudantil {
   public static void main(String[] args) {
-		int mediaFinal = 7;
+		int mediaFinal = 8;
 		if (mediaFinal < 6)
 			System.out.println("REPROVADO");
 		else if (mediaFinal == 6)
