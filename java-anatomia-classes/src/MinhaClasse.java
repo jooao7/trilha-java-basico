be266d208606eb154125f08881732bfe023bd29a
jooao7 <jvitorfjv781@gmail.com>
2024-05-03 15:49:02 -0300

    criando o projeto para exemplos de classes, tipos e variaveis

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,14 @@
+public class MinhaClasse {
+  public static void main (String [] args) {
+    String primeiroNome = "Will";
+    String segundoNome = "Smith";
+
+    String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
+  }
+
+  public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return primeiroNome.concat(" ").concat(segundoNome);
+  }
+
+}
