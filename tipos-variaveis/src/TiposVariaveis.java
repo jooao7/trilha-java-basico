bed8e85eb1d6aebbbc61a8a9e4915c04a68af50a
jooao7 <jvitorfjv781@gmail.com>
2024-05-03 16:54:51 -0300

    adiciona exemplos de operadores, tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Tipos primitivos
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 7;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+        
     }
 }
